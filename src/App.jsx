ad7ddae579967c5b25dcee545631172381e34237
jesusrobot <jesusrobot9000@gmail.com>
2022-10-08 19:09:51 -0600

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="container mx-auto mt-20">
-      <h1>Hola Mundo</h1>
+      <Header />
       <main className="mt-12 md:flex">
         <h2>Form</h2>
         <h2>Appointments</h2>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <h1 className="md:w-1/2 mx-auto text-5xl text-center font-black">
+      Seguimiento Pacientes {}
+      <span className="text-indigo-600">Veterinaria</span>
+    </h1>
+  )
+}
+
+export default Header
