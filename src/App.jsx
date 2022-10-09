172ceef903ef0c5f69d94600d8b1204e852141ab
jesusrobot <jesusrobot9000@gmail.com>
2022-10-08 19:06:51 -0600

    Added the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto mt-20">
       <h1>Hola Mundo</h1>
+      <main className="mt-12 md:flex">
+        <h2>Form</h2>
+        <h2>Appointments</h2>
+      </main>
     </div>
   )
 }
