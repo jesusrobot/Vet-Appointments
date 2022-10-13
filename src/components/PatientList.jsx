680281704352650e83909d9a6913a46001865e55
jesusrobot <jesusrobot9000@gmail.com>
2022-10-12 20:20:39 -0600

    shows a message in case of not having registered any appointment

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -4,14 +4,28 @@ import Patient from './Patient'
 function PatientList({ patients }) {
   return (
     <section className="md:h-screen overflow-y-scroll md:w-1/2 lg:w-3/5 ">
-      <h2 className="text-3xl text-center font-black">Listado Pacientes</h2>
-      <p className="mt-5 mb-10 text-lg text-center">
-        Administra tus {}
-        <span className="font-bold text-indigo-600">Pacientes y Citas</span>
-      </p>
-      {patients.map((patient) => (
-        <Patient key={patient.id} patient={patient} />
-      ))}
+      {patients.length > 0 ? (
+        <>
+          <h2 className="text-3xl text-center font-black">Listado Pacientes</h2>
+          <p className="mt-5 mb-10 text-lg text-center">
+            Administra tus {}
+            <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+          </p>
+          {patients.map((patient) => (
+            <Patient key={patient.id} patient={patient} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="text-3xl text-center font-black">
+            No hay citas de pacientes
+          </h2>
+          <p className="mt-5 mb-10 text-lg text-center">
+            Comienza agregando citas y {}
+            <span className="font-bold text-indigo-600">aparecerán aquí</span>
+          </p>
+        </>
+      )}
     </section>
   )
 }
